aac4000f6bc533365674a8e9d5d8ee44e88085c9
MaxDrachev <Maksimusdrachev1983@gmail.com>
2024-10-03 06:15:54 +0300

    fix DONE

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-<<<<<<< HEAD
 from catalog.views import home_page, contacts_page
-=======
->>>>>>> origin/main
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', home_page, name='home_page'),
     path('contacts/', contacts_page, name='contacts_page')
-=======
-    path('',)
->>>>>>> origin/main
 ]
